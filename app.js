73cad79ed5259b1410978f6332ee8079cb07cf7c
Ons <ons0x02@gmail.com>
2023-01-18 19:22:20 +0500

    Added error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); //middleware for logging functionality in terminal using (next) param
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-app.use('/products', productRoutes); //all incoming requests have to go through this method (middleware)
+app.use(morgan('dev'));
+
+//Routes to handle requests
+app.use('/products', productRoutes); 
 app.use('/orders', orderRoutes);
+
+//Error handling for requests that didn't make it to above routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error); //forward error request
+})
+
+//Error handling for requests that fail on server side instead of above
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
