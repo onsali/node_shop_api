bfeaf75a1f47ad9111623abc853aa4c241cb6c94
Ons <ons0x02@gmail.com>
2023-01-20 19:34:30 +0500

    Added populated queries for GET requests

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ router.get('/', (req, res, next) => {
     Order
         .find()
         .select('-__v')
+        .populate("product", "name")
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -76,6 +77,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
+    .populate("product")
     .exec()
     .then(order => {
         if (!order) {
